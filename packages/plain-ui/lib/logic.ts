833b1369341d9378e3bf5a1083c97c0bf3de9058
Jør∂¡ <itsbrickpop@gmail.com>
2022-12-06 10:57:49 +0000

    FINALLY WORKING

--- packages/plain-ui/lib/logic.ts
+++ packages/plain-ui/lib/logic.ts
@@ -248,7 +248,10 @@ export async function vote(
     voteValue,
     VOTE_WEIGHT,
     [proof.pi_a[0], proof.pi_a[1]],
-    [proof.pi_b[0], proof.pi_b[1]],
+    [
+      [proof.pi_b[0][1], proof.pi_b[0][0]],
+      [proof.pi_b[1][1], proof.pi_b[1][0]],
+    ],
     [proof.pi_c[0], proof.pi_c[1]],
   );
   const receipt = await tx.wait();
